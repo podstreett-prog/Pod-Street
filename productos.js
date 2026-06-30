b7a7cb22642e7179fc61cf7629e3a01311a635a4
podstreett-prog <pod.streett@gmail.com>
2026-06-30 15:26:14 +1200

    Create productos.js

---
+++ productos.js
@@ -0,0 +1,17 @@
+const productos = [
+  {
+    nombre: "Camiseta Negra",
+    categoria: "ropa",
+    precio: "50.000 Gs",
+    imagen: "https://via.placeholder.com/200",
+    whatsapp: "https://wa.me/595991735685?text=Quiero%20la%20camiseta%20negra"
+  },
+
+  {
+    nombre: "Audífonos Gamer",
+    categoria: "tecnologia",
+    precio: "120.000 Gs",
+    imagen: "https://via.placeholder.com/200",
+    whatsapp: "https://wa.me/595991735685?text=Quiero%20los%20audifonos"
+  }
+];
